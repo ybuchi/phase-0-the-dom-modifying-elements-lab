8c8644550a74c27f9956c24a9ad173876a02a4d9
ybuchi <yabuch_ch@hotmail.com>
2022-02-28 16:24:39 -0500

    commit tree c642cef2b81ba0bd288a053252801a9926b6c928

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+newElement = document.createElement('h1');
+newElement.id = 'victory'
+document.body.append(newElement)
+
+let newHeader = document.getElementById('victory');
